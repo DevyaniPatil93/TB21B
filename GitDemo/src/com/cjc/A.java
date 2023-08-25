b2163feca2162157ccef5830c8b03b717eadf8e5
DEXTER <DEXTER@DESKTOP-69GCDOM>
2023-08-25 10:59:30 +0530

    by dev 1

--- GitDemo/src/com/cjc/A.java
+++ GitDemo/src/com/cjc/A.java
@@ -1,8 +1,13 @@
 package com.cjc;
 
 public class A {
+	public void m1()
+	{
+		System.out.println("M1=A");
+	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("hello");
 
 	}
